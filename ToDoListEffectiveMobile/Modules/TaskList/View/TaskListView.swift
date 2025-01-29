60a10e1f8580a9c2cd93630edbe2090cfd417a41
uzakotim <timauzakov@gmail.com>
2025-01-29 18:20:49 +0500

    Refactoring: search bar

--- ToDoListEffectiveMobile/Modules/TaskList/View/TaskListView.swift
+++ ToDoListEffectiveMobile/Modules/TaskList/View/TaskListView.swift
@@ -1,5 +1,38 @@
 import SwiftUI
 
+struct SearchBar: View {
+    @Binding var searchText: String
+    @ObservedObject var presenter: TaskListPresenter
+    var body: some View {
+        HStack {
+            Spacer()
+            Image(systemName: "magnifyingglass") // Search icon
+                .foregroundColor(Color(UIColor.placeholderText))
+
+            TextField("Search", text: $searchText)
+                .textFieldStyle(PlainTextFieldStyle()) // Plain style for custom background
+                .cornerRadius(6)
+                .padding(.vertical, 8)  // Adjust vertical padding
+                .padding(.horizontal, 6)  // Adjust horizontal padding
+                .frame(maxWidth: .infinity)  // Set a fixed width for the TextField
+                .onChange(of: searchText) { oldValue, newValue in
+                    presenter.searchTasks(query: newValue)
+                }
+
+            Button(action: {
+                // Microphone button action here
+            }) {
+                Image(systemName: "mic.fill") // Microphone icon
+                    .foregroundColor(Color(UIColor.placeholderText))
+            }
+            Spacer()
+        }
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(12)
+        .padding(.horizontal, 16)
+    }
+}
+
 struct TaskListView: View {
     @ObservedObject var presenter: TaskListPresenter
     @State private var isNavigatingToTaskDetail = false
@@ -12,32 +45,7 @@ struct TaskListView: View {
             VStack {
                 // Main content
                 VStack {
-                    HStack {
-                        Spacer()
-                        Image(systemName: "magnifyingglass") // Search icon
-                            .foregroundColor(Color(UIColor.placeholderText))
-
-                        TextField("Search", text: $searchText)
-                            .textFieldStyle(PlainTextFieldStyle()) // Plain style for custom background
-                            .cornerRadius(6)
-                            .padding(.vertical, 8)  // Adjust vertical padding
-                            .padding(.horizontal, 6)  // Adjust horizontal padding
-                            .frame(maxWidth: .infinity)  // Set a fixed width for the TextField
-                            .onChange(of: searchText) { oldValue, newValue in
-                                presenter.searchTasks(query: newValue)
-                            }
-
-                        Button(action: {
-                            // Microphone button action here
-                        }) {
-                            Image(systemName: "mic.fill") // Microphone icon
-                                .foregroundColor(Color(UIColor.placeholderText))
-                        }
-                        Spacer()
-                    }
-                    .background(Color(.secondarySystemBackground))
-                    .cornerRadius(12)
-                    .padding(.horizontal, 16)  // Adjust padding around the HStack to control overall width
+                    SearchBar(searchText: $searchText, presenter: presenter)
                     List {
                         ForEach(presenter.filteredTasks) { task in
                             ListItemView(task: task)
