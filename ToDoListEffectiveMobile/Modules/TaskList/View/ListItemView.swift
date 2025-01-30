2ba25764a48060314abeca30e2d2fa08f480cb05
uzakotim <timauzakov@gmail.com>
2025-01-30 20:33:23 +0500

    bug with SwiftUI list fixed

--- ToDoListEffectiveMobile/Modules/TaskList/View/ListItemView.swift
+++ ToDoListEffectiveMobile/Modules/TaskList/View/ListItemView.swift
@@ -9,38 +9,40 @@ import SwiftUI
 struct ListItemView: View {
     var task: Task
     var body: some View {
-        VStack(alignment: .leading){
-            VStack(alignment: .leading) {
-                HStack{
-                    Image(systemName: task.isCompleted ? "checkmark.circle" : "circle")
-                        .foregroundColor(.yellow)
-                        .imageScale(.large)
-                    Text(task.title)
-                        .font(.headline)
-                        .strikethrough(task.isCompleted, color: Color(UIColor.placeholderText))
-                        .foregroundColor(task.isCompleted ? Color(UIColor.placeholderText) : .primary)
-                }
-                HStack{
-                    Image(systemName: "checkmark.circle")
-                        .imageScale(.large)
-                        .opacity(0)
-                    VStack(alignment: .leading) {
-                        
-                        Text(task.descriptionData)
-                            .font(.subheadline)
+        Section{
+            VStack(alignment: .leading){
+                VStack(alignment: .leading) {
+                    HStack{
+                        Image(systemName: task.isCompleted ? "checkmark.circle" : "circle")
+                            .foregroundColor(.yellow)
+                            .imageScale(.large)
+                        Text(task.title)
+                            .font(.headline)
+                            .strikethrough(task.isCompleted, color: Color(UIColor.placeholderText))
                             .foregroundColor(task.isCompleted ? Color(UIColor.placeholderText) : .primary)
-                        Text(task.dateCreatedFormatted)
-                            .font(.footnote)
-                            .foregroundColor(Color(UIColor.placeholderText))
                     }
+                    HStack{
+                        Image(systemName: "checkmark.circle")
+                            .imageScale(.large)
+                            .opacity(0)
+                        VStack(alignment: .leading) {
+                            
+                            Text(task.descriptionData)
+                                .font(.subheadline)
+                                .foregroundColor(task.isCompleted ? Color(UIColor.placeholderText) : .primary)
+                            Text(task.dateCreatedFormatted)
+                                .font(.footnote)
+                                .foregroundColor(Color(UIColor.placeholderText))
+                        }
+                    }
+                    Spacer()
                 }
-                Spacer()
-            }
-            .padding(0)
-            Divider()
-                .frame(height: 1)
-                .background(Color(UIColor.placeholderText))
                 .padding(0)
+                Divider()
+                    .frame(height: 1)
+                    .background(Color(UIColor.placeholderText))
+                    .padding(0)
+            }
         }
     }
 }
