51a5a04adad7009f7b7efb06f757c24e68dfe21c
uzakotim <timauzakov@gmail.com>
2025-01-31 20:19:16 +0500

    added testLoadTasks

--- ToDoListEffectiveMobileTests/ToDoListEffectiveMobileTests.swift
+++ ToDoListEffectiveMobileTests/ToDoListEffectiveMobileTests.swift
@@ -11,46 +11,32 @@ import XCTest
 final class ToDoListEffectiveMobileTests: XCTestCase {
 
     func testLoadTasks() {
-            // Create an expectation to wait for async task
+            // Create an expectation to wait for the completion handler
             let expectation = XCTestExpectation(description: "Tasks are fetched")
-            
-            var fetchedTasks = [Task]()
 
-            // Assuming you're testing the URLSession data task to fetch tasks
-            guard let url = URL(string: "https://dummyjson.com/todos") else {
-                XCTFail("Invalid URL")
-                return
-            }
-            
-            // Start the data task
-            URLSession.shared.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    XCTFail("Error fetching data: \(error)")
-                    return
+            var fetchedTasks = [Task]()
+            var fetchError: Error?
+            let interactor = TaskListInteractor()
+            // Call the fetchTasks method
+            interactor.fetchTasks { result in
+                switch result {
+                case .success(let tasks):
+                    fetchedTasks = tasks
+                case .failure(let error):
+                    fetchError = error
                 }
-                
-                guard let data = data else {
-                    XCTFail("No data returned")
-                    return
-                }
-                
-                do {
-                    let decodedResponse = try JSONDecoder().decode(TodosResponse.self, from: data)
-                    fetchedTasks = decodedResponse.todos
-                    print("Success")
-                } catch {
-                    XCTFail("Failed to decode JSON: \(error)")
-                }
-                
+
                 // Fulfill the expectation to signal that the async task is done
                 expectation.fulfill()
-            }.resume()
+            }
 
             // Wait for the expectation to be fulfilled or time out after 5 seconds
             wait(for: [expectation], timeout: 10)
 
+            // Assert that no error occurred
+            XCTAssertNil(fetchError, "Error fetching tasks: \(fetchError?.localizedDescription ?? "")")
+
             // Assert that the number of tasks is 30
             XCTAssertEqual(fetchedTasks.count, 30, "The number of tasks should be 30.")
         }
-
 }
