99e4c87b2d7883f89ba10823c2263c1365d53088
uzakotim <timauzakov@gmail.com>
2025-02-01 12:50:35 +0500

    Extensive commenting: SearchBar

--- ToDoListEffectiveMobile/Modules/TaskList/View/SearchBar.swift
+++ ToDoListEffectiveMobile/Modules/TaskList/View/SearchBar.swift
@@ -7,29 +7,30 @@
 import SwiftUI
 
 struct SearchBar: View {
-    @Binding var searchText: String
-    @ObservedObject var presenter: TaskListPresenter
+    @Binding var searchText: String // Связанное состояние текста поиска
+    @ObservedObject var presenter: TaskListPresenter // Презентер, управляющий поиском задач
     var body: some View {
         HStack {
-            Spacer()
-            Image(systemName: "magnifyingglass")
-                .foregroundColor(Color(UIColor.placeholderText))
+            Spacer() // Раздвижение элементов по краям
+            Image(systemName: "magnifyingglass") // Иконка поиска
+                .foregroundColor(Color(UIColor.placeholderText)) // Цвет как у placeholder-а
 
-            TextField("Поиск", text: $searchText)
-                .textFieldStyle(PlainTextFieldStyle())
-                .cornerRadius(6)
-                .padding(.vertical, 8)
-                .padding(.horizontal, 6)
-                .frame(maxWidth: .infinity)
+            TextField("Поиск", text: $searchText) // Поле ввода для поиска
+                .textFieldStyle(PlainTextFieldStyle()) // Стиль текстового поля
+                .cornerRadius(6) // Скругление углов
+                .padding(.vertical, 8) // Внутренний отступ сверху и снизу
+                .padding(.horizontal, 6) // Внутренний отступ слева и справа
+                .frame(maxWidth: .infinity) // Растяжение по ширине
                 .onChange(of: searchText) { oldValue, newValue in
-                    presenter.searchTasks(query: newValue)
+                    presenter.searchTasks(query: newValue) // Вызов метода поиска при изменении текста
                 }
-                .disableAutocorrection(true) 
-            Spacer()
+                .disableAutocorrection(true) // Отключение автокоррекции
+            
+            Spacer() // Раздвижение элементов по краям
         }
-        .background(Color(.secondarySystemBackground))
-        .cornerRadius(12)
-        .padding(.horizontal, 16)
+        .background(Color(.secondarySystemBackground)) // Фон элемента
+        .cornerRadius(12) // Скругление углов
+        .padding(.horizontal, 16) // Внешний отступ по горизонтали
     }
    
 }
