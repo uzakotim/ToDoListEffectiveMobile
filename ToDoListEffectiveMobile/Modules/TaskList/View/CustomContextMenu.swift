656089b569895823056f30a109f24d2fecc569d1
uzakotim <timauzakov@gmail.com>
2025-02-01 12:59:28 +0500

    Extensive commenting: ContextMenu

--- ToDoListEffectiveMobile/Modules/TaskList/View/CustomContextMenu.swift
+++ ToDoListEffectiveMobile/Modules/TaskList/View/CustomContextMenu.swift
@@ -6,33 +6,35 @@
 //
 import SwiftUI
 
+// Представление предпросмотра контекстного меню для задачи
 struct CustomContextMenuPreviewView: View {
-    let task: Task
+    let task: Task // Задача, информация о которой будет отображаться
 
     var body: some View {
         HStack{
             VStack(alignment: .leading, spacing: 2) {
-                Text(task.title)
-                    .font(.headline)
-                    .foregroundColor(.primary)
-                    .lineLimit(nil)
-                    .fixedSize(horizontal: false, vertical: true)
+                Text(task.title) // Заголовок задачи
+                    .font(.headline) // Стиль шрифта заголовка
+                    .foregroundColor(.primary) // Основной цвет текста
+                    .lineLimit(nil) // Без ограничения количества строк
+                    .fixedSize(horizontal: false, vertical: true) // Авторазмер по вертикали
                 
-                Text(task.descriptionData)
-                    .font(.subheadline)
-                    .foregroundColor(.primary)
-                    .lineLimit(nil)
-                    .fixedSize(horizontal: false, vertical: true)
+                Text(task.descriptionData) // Описание задачи
+                    .font(.subheadline) // Стиль шрифта описания
+                    .foregroundColor(.primary) // Основной цвет текста
+                    .lineLimit(nil) // Без ограничения количества строк
+                    .fixedSize(horizontal: false, vertical: true) // Авторазмер по вертикали
                 
-                Text(task.dateCreatedFormatted)
-                    .font(.caption)
-                    .foregroundColor(.gray)
+                
+                Text(task.dateCreatedFormatted) // Дата создания задачи
+                    .font(.caption) // Стиль шрифта для даты
+                    .foregroundColor(.gray) // Серый цвет для менее важной информации
             }
-            .padding(24)
-            Spacer()
+            .padding(24) // Внутренний отступ для содержимого
+            Spacer() // Раздвижение содержимого по горизонтали
         }
-        .padding(24)
-        .cornerRadius(12)
-        .frame(minWidth: 200, maxWidth: 400)
+        .padding(24) // Внешний отступ
+        .cornerRadius(12) // Скругление углов
+        .frame(minWidth: 200, maxWidth: 400) // Ограничение по ширине
     }
 }
